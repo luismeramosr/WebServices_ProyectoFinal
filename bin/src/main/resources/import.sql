59dcd03e9f8a91177ed31e959a7284c3a8ef0001
Luis Ramos <luismeramosr@gmail.com>
2021-07-03 12:48:51 -0500

    Backup commit, almost no changes from last commit

--- bin/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-use almacen;
-
-
-insert into `role` (id,`name`) values (1, "ADMIN");
-
-INSERT INTO almacen.`user` (id, dni, email, first_name, id_role, last_name, password, phone, username) VALUES('O00001', 98765432, 'test@mail.com', 'Luis', 1, 'Ramos', '$2a$10$GSeYUCLs6kwEm6MMXdj7EOLzF7TOMVcxMLksldDv6/81OLh6qXlwa', 123456789, '123');
